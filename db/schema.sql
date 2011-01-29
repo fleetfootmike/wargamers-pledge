fd3e80788de2a7db26d85847e5329cfc74e38ce1
Mike Whitaker <mike@altrion.org>
2011-01-29 09:13:29 +0000

    Adding email field for "user email" story Also saving SQLEditor source file in repo.

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,8 @@
 
 We'll add other authtypes and extra columns as needed to handle external auth.
 user.id will be used in user-specific URLs.
+
+user.email is optional.
 */
 
 CREATE TABLE user
@@ -10,6 +12,7 @@ CREATE TABLE user
     userid VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
     authtype ENUM('local') DEFAULT 'local',
+    email VARCHAR(255), 
     PRIMARY KEY (id)
 );
 
