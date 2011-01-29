163bd36de43c16615eac0e68e0516a48378ae65d
Mike Whitaker <mike@altrion.org>
2011-01-29 11:53:26 +0000

    event and figures tables for "Buy", "Paint" and "stats" user stories

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,40 @@ user.id will be used in user-specific URLs.
 
 user.email is optional.
 */
-
 CREATE TABLE user
 (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
     userid VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
     authtype ENUM('local') DEFAULT 'local',
-    email VARCHAR(255), 
-    PRIMARY KEY (id)
-);
+    email VARCHAR(255),
+PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
 
+CREATE TABLE figures
+(
+    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    status ENUM('bought','painted') NOT NULL,
+    number INTEGER NOT NULL,
+    owner INTEGER NOT NULL,
+    notes TINYTEXT,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE event
+(
+    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    status ENUM('bought','painted') NOT NULL,
+    user INTEGER NOT NULL,
+    figures INTEGER NOT NULL,
+    date DATE NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE INDEX userid_idx ON user(userid);
+ALTER TABLE figures ADD FOREIGN KEY owner_idxfk (owner) REFERENCES user (id);
+
+CREATE INDEX status_idx ON event(status);
+ALTER TABLE event ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
+
+ALTER TABLE event ADD FOREIGN KEY figures_idxfk (figures) REFERENCES figures (id);
