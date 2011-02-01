e1817e5aea25641cf806dc73a00b77520ebfae26
Mike Whitaker <mike@altrion.org>
2011-02-01 16:09:50 +0000

    user table - ditch integer primary key, adjust accordingly

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,7 @@
 
 CREATE TABLE user
 (
-id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-name VARCHAR(255),
+id VARCHAR(255) NOT NULL,
 email VARCHAR(255),
 created DATETIME,
 last DATETIME,
@@ -12,8 +11,7 @@ PRIMARY KEY (id)
 
 CREATE TABLE auth_password
 (
-user INTEGER,
-login VARCHAR(255) NOT NULL UNIQUE,
+user VARCHAR(255),
 password VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
@@ -31,17 +29,16 @@ CREATE TABLE event
 (
 id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 status ENUM('bought','painted') NOT NULL,
-user INTEGER NOT NULL,
+user VARCHAR(255) NOT NULL,
 figures INTEGER NOT NULL,
 date DATE NOT NULL,
 PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
+CREATE INDEX id_idx ON user(id);
 ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
 
 CREATE INDEX password_idx ON auth_password(password);
-ALTER TABLE figures ADD FOREIGN KEY owner_idxfk (owner) REFERENCES user (id);
-
 CREATE INDEX status_idx ON event(status);
 ALTER TABLE event ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
 
