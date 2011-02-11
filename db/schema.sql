900a29b6ecd070db7aa24acae9bdf90703b22306
Mike Whitaker <mike@altrion.org>
2011-02-11 13:21:36 +0000

    Add manufacturer, figure tables. Amend purchase and event tables.

--- db/schema.sql
+++ db/schema.sql
@@ -15,23 +15,42 @@ user VARCHAR(255),
 password VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
-CREATE TABLE figures
+CREATE TABLE purchase
 (
 id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-status ENUM('bought','painted') NOT NULL,
-number INTEGER NOT NULL,
-owner INTEGER NOT NULL,
+num INTEGER NOT NULL,
+user INTEGER NOT NULL,
+manufacturer VARCHAR(255) COMMENT 'Autocomplete from manufacturer table',
+description VARCHAR(255) COMMENT 'Autocomplete from figure table for given manufacturer',
+scale VARCHAR(15),
+acquired DATE,
 notes TINYTEXT,
 PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
-CREATE TABLE event
+CREATE TABLE action
 (
 id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-status ENUM('bought','painted') NOT NULL,
+purchase INTEGER NOT NULL COMMENT '...I want to paint some of these....',
+use_as VARCHAR(255) COMMENT '...as Balearic slingers...',
+num INTEGER,
 user VARCHAR(255) NOT NULL,
-figures INTEGER NOT NULL,
-date DATE NOT NULL,
+when DATE NOT NULL,
+notes TINYTEXT,
+PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE manufacturer
+(
+id VARCHAR(255) UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE figure
+(
+id VARCHAR(255) UNIQUE,
+manufacturer VARCHAR(255),
+scale VARCHAR(15),
 PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
@@ -39,7 +58,12 @@ CREATE INDEX id_idx ON user(id);
 ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
 
 CREATE INDEX password_idx ON auth_password(password);
-CREATE INDEX status_idx ON event(status);
-ALTER TABLE event ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
+ALTER TABLE purchase ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
 
-ALTER TABLE event ADD FOREIGN KEY figures_idxfk (figures) REFERENCES figures (id);
+CREATE INDEX manufacturer_idx ON purchase(manufacturer);
+CREATE INDEX scale_idx ON purchase(scale);
+CREATE INDEX use_as_idx ON action(use_as);
+ALTER TABLE action ADD FOREIGN KEY user_idxfk_2 (user) REFERENCES user (id);
+
+CREATE INDEX manufacturer_idx ON figure(manufacturer);
+ALTER TABLE figure ADD FOREIGN KEY manufacturer_idxfk (manufacturer) REFERENCES manufacturer (id);
