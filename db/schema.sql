1f2327037230bb6eb04d0d89b8fd96b21b150fe1
Mike Whitaker <mike@altrion.org>
2011-02-27 20:13:49 +0000

    Add auth_twitter table as per Catalyst::Auth::Credential::Twitter

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,27 @@
 /* SQLEditor (MySQL (2))*/
 
+CREATE TABLE manufacturer
+(
+id VARCHAR(255) UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE figure
+(
+id VARCHAR(255) UNIQUE,
+manufacturer VARCHAR(255),
+scale VARCHAR(15),
+PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE auth_twitter
+(
+twitter_user VARCHAR(255),
+twitter_user_id VARCHAR(255) UNIQUE,
+twitter_access_token VARCHAR(255),
+twitter_access_token_secret VARCHAR(255)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
 CREATE TABLE user
 (
 id VARCHAR(255) NOT NULL UNIQUE,
@@ -7,19 +29,6 @@ email VARCHAR(255),
 created DATETIME,
 last DATETIME,
 PRIMARY KEY (id)
-) ENGINE=InnoDB CHARACTER SET=utf8;
-
-CREATE TABLE manufacturer
-(
-id VARCHAR(255) UNIQUE,
-PRIMARY KEY (id)
-) ENGINE=InnoDB CHARACTER SET=utf8;
-
-CREATE TABLE auth_password
-(
-user VARCHAR(255) UNIQUE,
-password VARCHAR(255) NOT NULL,
-PRIMARY KEY (user)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
 CREATE TABLE purchase
@@ -48,26 +57,28 @@ action ENUM('painted'),
 PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
-CREATE TABLE figure
+CREATE TABLE auth_password
 (
-id VARCHAR(255) UNIQUE,
-manufacturer VARCHAR(255),
-scale VARCHAR(15),
-PRIMARY KEY (id)
+user VARCHAR(255) UNIQUE,
+password VARCHAR(255) NOT NULL,
+PRIMARY KEY (user)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
-CREATE INDEX id_idx ON user(id);
-ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
+CREATE INDEX manufacturer_idx ON figure(manufacturer);
+ALTER TABLE figure ADD FOREIGN KEY manufacturer_idxfk (manufacturer) REFERENCES manufacturer (id);
 
-CREATE INDEX password_idx ON auth_password(password);
-ALTER TABLE purchase ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
+CREATE INDEX id_idx ON user(id);
+ALTER TABLE user ADD FOREIGN KEY id_idxfk (id) REFERENCES auth_twitter (twitter_user_id);
+
+ALTER TABLE purchase ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
 
 CREATE INDEX manufacturer_idx ON purchase(manufacturer);
 CREATE INDEX scale_idx ON purchase(scale);
 ALTER TABLE action ADD FOREIGN KEY purchase_idxfk (purchase) REFERENCES purchase (id);
 
 CREATE INDEX use_as_idx ON action(use_as);
-ALTER TABLE action ADD FOREIGN KEY user_idxfk_2 (user) REFERENCES user (id);
+ALTER TABLE action ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
 
-CREATE INDEX manufacturer_idx ON figure(manufacturer);
-ALTER TABLE figure ADD FOREIGN KEY manufacturer_idxfk (manufacturer) REFERENCES manufacturer (id);
+ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk_2 (user) REFERENCES user (id);
+
+CREATE INDEX password_idx ON auth_password(password);
