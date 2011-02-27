9190e86a4f135509fe069dc3a724ac37a7aa482c
Mike Whitaker <mike@altrion.org>
2011-02-27 20:27:49 +0000

    Add user row to auth_twitter as foreign key ref to main user table

--- db/schema.sql
+++ db/schema.sql
@@ -14,14 +14,6 @@ scale VARCHAR(15),
 PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
-CREATE TABLE auth_twitter
-(
-twitter_user VARCHAR(255),
-twitter_user_id VARCHAR(255) UNIQUE,
-twitter_access_token VARCHAR(255),
-twitter_access_token_secret VARCHAR(255)
-) ENGINE=InnoDB CHARACTER SET=utf8;
-
 CREATE TABLE user
 (
 id VARCHAR(255) NOT NULL UNIQUE,
@@ -29,6 +21,16 @@ email VARCHAR(255),
 created DATETIME,
 last DATETIME,
 PRIMARY KEY (id)
+) ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE auth_twitter
+(
+user VARCHAR(255) UNIQUE,
+twitter_user VARCHAR(255),
+twitter_user_id VARCHAR(255) UNIQUE,
+twitter_access_token VARCHAR(255),
+twitter_access_token_secret VARCHAR(255),
+PRIMARY KEY (user)
 ) ENGINE=InnoDB CHARACTER SET=utf8;
 
 CREATE TABLE purchase
@@ -68,17 +70,19 @@ CREATE INDEX manufacturer_idx ON figure(manufacturer);
 ALTER TABLE figure ADD FOREIGN KEY manufacturer_idxfk (manufacturer) REFERENCES manufacturer (id);
 
 CREATE INDEX id_idx ON user(id);
-ALTER TABLE user ADD FOREIGN KEY id_idxfk (id) REFERENCES auth_twitter (twitter_user_id);
+CREATE INDEX user_idx ON auth_twitter(user);
+ALTER TABLE auth_twitter ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
 
-ALTER TABLE purchase ADD FOREIGN KEY user_idxfk (user) REFERENCES user (id);
+CREATE INDEX twitter_user_id_idx ON auth_twitter(twitter_user_id);
+ALTER TABLE purchase ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
 
 CREATE INDEX manufacturer_idx ON purchase(manufacturer);
 CREATE INDEX scale_idx ON purchase(scale);
 ALTER TABLE action ADD FOREIGN KEY purchase_idxfk (purchase) REFERENCES purchase (id);
 
 CREATE INDEX use_as_idx ON action(use_as);
-ALTER TABLE action ADD FOREIGN KEY user_idxfk_1 (user) REFERENCES user (id);
+ALTER TABLE action ADD FOREIGN KEY user_idxfk_2 (user) REFERENCES user (id);
 
-ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk_2 (user) REFERENCES user (id);
+ALTER TABLE auth_password ADD FOREIGN KEY user_idxfk_3 (user) REFERENCES user (id);
 
 CREATE INDEX password_idx ON auth_password(password);
