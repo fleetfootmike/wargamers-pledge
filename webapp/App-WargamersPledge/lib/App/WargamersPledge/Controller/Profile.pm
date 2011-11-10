a7183bbfd7623726dbf335deddecdec77fcbc4da
David Dorward <david@dorward.me.uk>
2011-11-10 20:50:58 +0000

    User profile related changes
    I put this project down for too long and have no idea why I didn't
    check these changes in at the time. They are going in now and if I find
    problems with them, that can be fixed later.

--- webapp/App-WargamersPledge/lib/App/WargamersPledge/Controller/Profile.pm
+++ webapp/App-WargamersPledge/lib/App/WargamersPledge/Controller/Profile.pm
@@ -23,10 +23,46 @@ Catalyst Controller.
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-
     $c->response->body('Matched App::WargamersPledge::Controller::Profile in Profile.');
 }
 
+sub user :Path :Args(1) {
+    my ( $self, $c, $user_id ) = @_;
+    my $user = $c->model('API')->get_profile($user_id);
+    $c->detach('Root', 'not_found') unless $user;
+    
+    # User profile
+    
+    use Data::Dump qw/dump/;
+    
+    warn dump $user;
+    
+    # Statistics
+    
+    my $all_time = $c->model('API')->get_stats($user_id);
+    $all_time->{label} = "All Time";
+    my $stats = [$all_time];
+    
+    # Recent purchases
+    
+    
+    
+    $c->stash(
+              user => $user_id,
+              stats => $stats
+              );
+    
+    # TODO
+    # Purchases - Paints since start of year
+    # Purchases - Paints since now - 12 months
+    # Most recent purchases
+    # Most recent paints
+    # Most recent photos
+    # User profile box
+    
+    #use Data::Dump qw/dump/;
+    #$c->response->body("Showing $user_id <pre>" . dump $stats);
+}
 
 =head1 AUTHOR
 
